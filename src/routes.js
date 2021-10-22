4a6c0d4f9a3e54df936c8106af3057ddb74beff2
Matheus <matheusbastos@cpejr.com.br>
2021-10-22 19:13:16 -0300

    Criei as rotas
    Estão todas comentadas pois o controller ainda não está criado, então comentamos para não crashar

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+// const express = require('express');
+// const routes = express.Router();
+
+// const comentarioController = require("./controllers/comentarioController");
+// const profissionalController = require("./controllers/profissionalController");
+// const servicoController = require("./controllers/servicoController");
+
+// //servico
+// routes.get('/servico/:servicoId', servicoController.getById);
+// routes.post('/servico', servicoController.create);
+// routes.put('/servico/:servicoId', servicoController.update);
+// routes.delete('/servico/:servicoId', servicoController.delete);
+
+// //profissional
+// routes.get('/profissional/:profissionalId', profissionalController.getById);
+// routes.post('/profissional', profissionalController.create);
+// routes.put('/profissional/:profissionalId', profissionalController.update);
+// routes.delete('/profissional/:profissionalId', profissionalController.delete);
+
+// //comentario
+// routes.get('/comentario/:comentarioId', comentarioController.getById);
+// routes.post('/comentario', comentarioController.create);
+// routes.put('/comentario/:comentarioId', comentarioController.update);
+// routes.delete('/comentario/:comentarioId', comentarioController.delete);
+
+// module.exports = routes;
+
