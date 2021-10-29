2043880f94b261b918862e77c7b03519fc2e823d
Gabriela Ribeiro dos Santos <rs.gabriela29@gmail.com>
2021-10-29 17:40:43 -0300

    codigo validator serviço

--- src/validators/servicoValidator.js
+++ src/validators/servicoValidator.js
@@ -8,19 +8,31 @@ module.exports = {
 
             nome : Joi.string().required(),
             especificaçoes : Joi.string().required(),
-        })
+        }),
         
     }),
     getById: celebrate({
-        [Segments.BODY] : Joi.object().keys({})
+        [Segments.PARAMS] : Joi.object().keys({
+            servico_id : Joi.string().required(),
+
+        }),
 
     }),
     update: celebrate({
-        [Segments.BODY] : Joi.object().keys({})
+        [Segments.PARAMS] :Joi.object().keys({
+            servico_id : Joi.string().required(),   
+        }),
+
+        [Segments.BODY] : Joi.object().keys({
+            nome : Joi.string().optional(),
+            especificaçoes : Joi.string().optional(), 
+        }),
     
     }),
     delete: celebrate({
-        [Segments.BODY] : Joi.object().keys({})
+        [Segments.PARAMS] :Joi.object().keys({
+            servico_id : Joi.string().required(),   
+        }),
 
     }),
 
